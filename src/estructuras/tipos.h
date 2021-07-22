31c25b0980c7590c3cedc6e7868e9df63fe50d28
santicoronel <santi.loga00@gmail.com>
2021-07-22 12:03:59 -0300

    Comenta y cierra modulo 'tipos'.

--- src/estructuras/tipos.h
+++ src/estructuras/tipos.h
@@ -1,15 +1,16 @@
+/** Tipos utilizados en las estructuras de datos. */
+
 #ifndef __TIPOS_H__
 #define __TIPOS_H__
 
-typedef int (*FuncionComparadora)(void* dato1, void* dato2);
 /** Retorna un entero negativo si dato1 < dato2, 0 si son iguales y un entero
  * positivo si dato1 > dato2  */
-typedef void (*FuncionDestructora)(void* dato);
+typedef int (*FuncionComparadora)(void* dato1, void* dato2);
 /** Libera la memoria alocada para el dato */
-typedef unsigned (*FuncionHash)(void* dato);
+typedef void (*FuncionDestructora)(void* dato);
 /** Retorna un entero sin signo para el dato */
+typedef unsigned (*FuncionHash)(void* dato);
+/** Visita el elemento, utilizando un dato extra en la llamada */
 typedef void (*FuncionVisitante)(void* dato, void* extra);
-
-typedef void* (*FuncionClave)(void* dato);
 
 #endif /* __TIPOS_H__ */
