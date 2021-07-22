dd255900c71b54158ca0517deed0d2d4249256f3
santicoronel <santi.loga00@gmail.com>
2021-07-22 11:41:50 -0300

    Comenta y cierra modulo 'pila'.

--- src/estructuras/pila.c
+++ src/estructuras/pila.c
@@ -1,20 +1,29 @@
+/** Implementa una pila como una lista circular, utilizando arreglos estaticos. */
+
 #include "pila.h"
 
 #include <stdlib.h>
 #include <assert.h>
 
+/**
+ * Estructura de la pila. Tenemos un array de elementos, su capacidad,
+ * la posicion del elemento del tope y una funcion destructora.
+ * Representamos una posicion vacia con NULL.
+ */
 struct _Pila {
-  int capacidad;
   void** elems;
+  int capacidad;
   int top;
   FuncionDestructora destr;
 };
 
+/** Devuelve un puntero al tope de la pila. */  
 static void** top(Pila pila) { return pila->elems + pila->top; }
 
 Pila pila_crear(int capacidad, FuncionDestructora destr) {
   Pila pila = malloc(sizeof(*pila)); assert(pila);
   void** elems = calloc(capacidad, sizeof(*elems));
+  // El ultimo es la posicion anterior a la inicial, modulo la capacidad.
   *pila = (struct _Pila) {capacidad, elems, capacidad - 1, destr};
   return pila;
 }
@@ -27,6 +36,7 @@ void pila_destruir(Pila pila) {
 
 void pila_push(Pila pila, void* dato) {
   pila->top = (pila->top + 1) % pila->capacidad;
+  // Si la pila esta llena, borra el elemento mas viejo.
   if (*top(pila)) pila->destr(*top(pila));
   *top(pila) = dato;
 }
--- src/estructuras/pila.h
+++ src/estructuras/pila.h
@@ -1,3 +1,5 @@
+/** Implementa una pila. */
+
 #ifndef __PILA_H__
 #define __PILA_H__
 
@@ -5,10 +7,27 @@
 
 typedef struct _Pila* Pila;
 
+/**
+ * Crea una pila con la capacidad y funcion destructoras dadas.
+ * La capacidad es estatica y a medidad que se inserten nuevos elementos
+ * se eliminaran los mas viejos.
+ */
 Pila pila_crear(int capacidad, FuncionDestructora destr);
+/**
+ * Destruye la pila.
+ */
 void pila_destruir(Pila pila);
+/**
+ * Inserta un elemento en el tope de la pila.
+ */
 void pila_push(Pila pila, void* dato);
+/**
+ * Quita el elemento del tope de la pila y lo devuelve.
+ */
 void* pila_pop(Pila pila);
+/**
+ * Vacia la pila, destruyendo todos sus elementos.
+ */
 void pila_vaciar(Pila pila);
 
-#endif /* __PILA_H__ */
+#endif /** __PILA_H__ */
