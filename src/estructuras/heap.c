28d4cef02e89972b2aedca3fecf50d7997ec92d1
santicoronel <santi.loga00@gmail.com>
2021-07-22 11:32:52 -0300

    Comenta y cierra modulo 'heap'.

--- src/estructuras/heap.c
+++ src/estructuras/heap.c
@@ -3,6 +3,10 @@
 #include <stdlib.h>
 #include <assert.h>
 
+/**
+ * Estructura del heap. Tenemos un array de elementos, la capacidad del heap,
+ * la posicion del ultimo elemento y nuestra funcion comparadora.
+ */
 struct _Heap {
   void** elems;
   unsigned int capacidad;
@@ -13,7 +17,10 @@ struct _Heap {
 Heap heap_crear(unsigned int capacidad, 
   FuncionComparadora comp) {
   Heap heap = malloc(sizeof(*heap)); assert(heap);
+  // Reservamos un espacio de mas para dejar el 0 vacio.
   void** elems = malloc(sizeof(*elems) * (capacidad + 1)); assert(elems);
+  elems[0] = NULL;
+  // Si no hay ningun elemento, decimos que el ultimo es 0.
   *heap = (struct _Heap) {elems, capacidad, 0, comp};
   return heap;
 }
@@ -27,9 +34,11 @@ int heap_vacio(Heap heap) {
 }
 
 int heap_insertar(void* dato, Heap heap) {
-  if (heap->ultimo == heap->capacidad) return 0;
+  if (heap->ultimo == heap->capacidad) return 0;  // El heap esta lleno
+  // Aumentamos el ultimo en 1 e insertamos el nuevo elemento al final.
   heap->elems[++heap->ultimo] = dato;
   unsigned int i = heap->ultimo;
+  // El nuevo elemento trepa hasta una posicion adecuada.
   while (i > 1 && heap->comp(dato, heap->elems[i / 2]) < 0) {
     heap->elems[i] = heap->elems[i / 2];  
     i /= 2; heap->elems[i] = dato;
@@ -38,12 +47,14 @@ int heap_insertar(void* dato, Heap heap) {
 }
 
 void* heap_extraer(Heap heap) {
+  // Extraemos el primer elemento y lo reemplazamos con el ultimo.
   void* top = heap->elems[1];
   heap->elems[1] = heap->elems[heap->ultimo--];
   
   unsigned int j = 1; int esMayor = 1; void* temp;
   FuncionComparadora comp = heap->comp;
   void** elems = heap->elems;
+  // El primer elemento se hunde hasta una posicion adecuada.
   while (j * 2 <= heap->ultimo && esMayor) {
     unsigned int hijo = j * 2;
     if (hijo + 1  <= heap->ultimo && comp(elems[hijo], elems[hijo + 1]) > 0)
--- src/estructuras/heap.h
+++ src/estructuras/heap.h
@@ -1,3 +1,5 @@
+/** Implementa una cola de prioridad. */
+
 #ifndef __HEAP_H__
 #define __HEAP_H__
 
@@ -5,15 +7,29 @@
 
 typedef struct _Heap* Heap;
 
+/**
+ * Crea una cola de prioridad con la capacidad y funcion comparadora dadas.
+ * Siempre tendra mayor prioridad el menor elemento.
+ */
 Heap heap_crear(unsigned int capacidad, 
   FuncionComparadora comp);
-
+/**
+ * Destruye el heap.
+ */
 void heap_destruir(Heap heap);
-
+/**
+ * Determina si el heap esta vacio.
+ */
 int heap_vacio(Heap heap);
-
+/**
+ * Inserta un elemento en el heap. Devuelve 1 si se pudo realizar la operacion,
+ * 0 en caso contrario.
+ */
 int heap_insertar(void* dato, Heap heap);
-
+/**
+ * Extrae el elemento de mayor prioridad del heap. 
+ * Devuelve NULL si el heap esta vacio.
+ */
 void* heap_extraer(Heap heap);
 
-#endif /* __HEAP_H__ */
+#endif /** __HEAP_H__ */
