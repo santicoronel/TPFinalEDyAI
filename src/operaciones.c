d545a5038fe6453cb3731505b78147a450724637
santicoronel <santi.loga00@gmail.com>
2021-07-23 15:00:13 -0300

    Comenta y cierra modulo 'operaciones'.

--- src/operaciones.c
+++ src/operaciones.c
@@ -9,14 +9,16 @@
 #include "historial.h"
 #include "utils.h"
 #include "estructuras/heap.h"
-#include "estructuras/pila.h"
 
-#define STRLEN 50
+#define STRLEN 50 // Macro para largo de strings.
 
+/** Nombres de atributos de contacto. */
 char const* const atributosClave[CANT_ATRIBUTOS] = 
 	{"nombre", "apellido", "edad", "telefono"};
 
+
 Resultado buscar(Entorno entorno) {
+  // Crea un contacto auxiliar para hacer la busqueda.
   char* nombre = ingresar_string(atributosClave[NOMBRE], STRLEN); 
   char* apellido = ingresar_string(atributosClave[APELLIDO], STRLEN);
   Contacto dummy = contacto_crear(nombre,apellido, -1, NULL);
@@ -29,6 +31,7 @@ Resultado buscar(Entorno entorno) {
   return EXITO;
 }
 
+
 Resultado agregar(Entorno entorno) {
   char* nombre = ingresar_string(atributosClave[NOMBRE], STRLEN);
   char* apellido = ingresar_string(atributosClave[APELLIDO], STRLEN);
@@ -37,30 +40,36 @@ Resultado agregar(Entorno entorno) {
   scanf("%u", &edad); while(getchar() != '\n');
   char* telefono = ingresar_string(atributosClave[TELEFONO], STRLEN);
   Contacto contacto = contacto_crear(nombre, apellido, edad, telefono);
+  // Si el contacto ya existia, lo destruimos e informamos el error.
   if (!tablahash_insertar(entorno.agenda, contacto)) {
     contacto_destruir(contacto); 
     return AGREGAR_EXISTE;
-  }; 
+  };
+  // Agregamos la operacion realizada al historial.
   historial_hecho(entorno.historial, operacion_crear(AGREGAR, contacto));
   
   return EXITO;
 }
 
+
 Resultado eliminar(Entorno entorno) {
+  // Crea un contacto auxiliar para hacer la busqueda.
   char* nombre = ingresar_string(atributosClave[NOMBRE], STRLEN); 
   char* apellido = ingresar_string(atributosClave[APELLIDO], STRLEN);
   Contacto dummy = contacto_crear(nombre,apellido, -1, NULL);
   Contacto eliminado = tablahash_eliminar(entorno.agenda, dummy);
   contacto_destruir(dummy);
-
+  // Si el contacto existia, agregamos la operacion realizada al historial.
   if (eliminado)
     historial_hecho(entorno.historial, operacion_crear(ELIMINAR, eliminado));
-  else return ELIMINAR_EXISTE;
+  else return ELIMINAR_EXISTE; // Informamos que el contacto no fue encontrado.
 
   return EXITO;
 }
 
+
 Resultado editar(Entorno entorno) {
+  // Crea un contacto auxiliar para hacer la busqueda.
   char* nombre = ingresar_string(atributosClave[NOMBRE], STRLEN); 
   char* apellido = ingresar_string(atributosClave[APELLIDO], STRLEN);
   Contacto dummy = contacto_crear(nombre,apellido, -1, NULL);
@@ -69,7 +78,8 @@ Resultado editar(Entorno entorno) {
     contacto_destruir(dummy);
     return EDITAR_EXISTE;
   }
-
+  // Si el contacto existe, reemplaza los datos y guarda los datos viejos
+  // en el historial.
   dummy->edad = contacto->edad;
   dummy->telefono = contacto->telefono;
   int edad;
@@ -77,27 +87,30 @@ Resultado editar(Entorno entorno) {
   scanf("%u", &edad); while(getchar() != '\n');
   contacto->edad = edad;
   contacto->telefono = ingresar_string(atributosClave[TELEFONO], STRLEN);
-  
   historial_hecho(entorno.historial, operacion_crear(EDITAR, dummy));
 
   return EXITO;
 }
 
+
+/** Chequea que la cabecera del archivo sea valida */
 static Resultado cabecera_valida(FILE* fp) {
   char input[STRLEN];
   for (int i = 0; i < CANT_ATRIBUTOS; i++) {
     switch (leer_palabra(fp, STRLEN, input)) {
     case COMA:
+      // Si es la ultima palabra esperamos un salto de linea, no una coma.
       if (i == CANT_ATRIBUTOS - 1) return CARGAR_COMA;
       if ((strcmp(input, atributosClave[i]) != 0)) return CARGAR_CABECERA;
       break;
     case SALTO_LINEA:
+      // Si no es la ultima palabra, esperamos una coma, no un salto de linea.
       if (i != CANT_ATRIBUTOS - 1) return CARGAR_SALTO_LINEA;
       if ((strcmp(input, atributosClave[i]) != 0)) return CARGAR_CABECERA;
       break;
     case FINAL:
       if (i == 0) return CARGAR_VACIO;
-      //fallthrough
+      // fallthrough
     case ERROR_EOF:
       return CARGAR_EOF;
       break;
@@ -132,11 +145,13 @@ Resultado cargar(Entorno entorno) {
       }
       switch(lec) {
       case COMA:
+        // Si es la ultima palabra esperamos un salto de linea, no una coma.
         if (i == CANT_ATRIBUTOS - 1) {
           res = CARGAR_COMA; seguir = 0;
         }
         break;
       case SALTO_LINEA:
+        // Si no es la ultima palabra, esperamos una coma, no un salto de linea.
         if (i != CANT_ATRIBUTOS - 1) {
           res = CARGAR_SALTO_LINEA; seguir = 0;
         }
@@ -174,6 +189,8 @@ Resultado cargar(Entorno entorno) {
   return res;
 }
 
+
+/** Funcion visitante para escribir contacto en archivo. */
 static void escribir_contacto_general(void* contacto, void* fp) {
   escribir_contacto(contacto, fp);
 }
@@ -188,20 +205,18 @@ Resultado guardar(Entorno entorno) {
   return EXITO;
 }
 
+
 Resultado deshacer(Entorno entorno) {
   Operacion operacion = historial_ultimo_hecho(entorno.historial);
   if (operacion == NULL) return DESHACER_VACIO;
-
-  Contacto dummy = contacto_crear(
-    operacion->contacto->nombre,
-    operacion->contacto->apellido,
-    -1, NULL);
   switch (operacion->tag) {
     case AGREGAR:
-      tablahash_eliminar(entorno.agenda, dummy);
+      // Eliminamos el contacto de la agenda.
+      tablahash_eliminar(entorno.agenda, operacion->contacto);
       break;
     case EDITAR: {
-      Contacto contacto = tablahash_buscar(entorno.agenda, dummy);
+      // Intercambiamos los datos de los contactos.
+      Contacto contacto = tablahash_buscar(entorno.agenda, operacion->contacto);
       unsigned int edad = operacion->contacto->edad; 
       char* telefono = operacion->contacto->telefono; 
       operacion->contacto->edad = contacto->edad;
@@ -210,50 +225,49 @@ Resultado deshacer(Entorno entorno) {
       contacto->telefono = telefono;
       break; }
     case ELIMINAR:
+      // Agregamos el contacto eliminado a la agenda.
       tablahash_insertar(entorno.agenda, operacion->contacto);
       break; 
   }
+  // Agregamos la operacion deshecha al historial
   historial_deshecho(entorno.historial, operacion);
-  dummy->nombre = NULL; dummy->apellido = NULL;
-  contacto_destruir(dummy);
 
   return EXITO;
 }
+
 
 Resultado rehacer(Entorno entorno) {
   Operacion operacion = historial_ultimo_deshecho(entorno.historial);
   if (operacion == NULL) return REHACER_VACIO;
 
-  Contacto dummy = contacto_crear(
-    operacion->contacto->nombre,
-    operacion->contacto->apellido,
-    -1, NULL);
   switch (operacion->tag) {
     case AGREGAR:
+      // Volvemos a agregar el contacto a la agenda.
       tablahash_insertar(entorno.agenda, operacion->contacto);
-      historial_hecho(entorno.historial, operacion);
       break;
     case EDITAR: {
-      Contacto contacto = tablahash_buscar(entorno.agenda, dummy);
+      // Intercambiamos los datos de los contactos.
+      Contacto contacto = tablahash_buscar(entorno.agenda, operacion->contacto);
       unsigned int edad = operacion->contacto->edad; 
       char* telefono = operacion->contacto->telefono; 
       operacion->contacto->edad = contacto->edad;
       operacion->contacto->telefono = contacto->telefono;
       contacto->edad = edad;
       contacto->telefono = telefono;
-      historial_hecho(entorno.historial, operacion);
       break; }
     case ELIMINAR:
+      // Volvemos a eliminar el contacto de la agenda.
       tablahash_eliminar(entorno.agenda, operacion->contacto);
-      historial_hecho(entorno.historial, operacion);
       break; 
   }
-  dummy->nombre = NULL; dummy->apellido = NULL;
-  contacto_destruir(dummy);  
+  // Agregamos la operacion realizada al historial.
+  historial_hecho(entorno.historial, operacion);
 
   return EXITO;
 }
 
+
+/** Funcion auxiliar que implementa operaciones AND/OR. */
 static Resultado and_or(Entorno entorno, FuncionVisitante imprimir_contacto) {
   char* input[CANT_ATRIBUTOS];
   unsigned int edad;
@@ -280,7 +294,7 @@ static Resultado and_or(Entorno entorno, FuncionVisitante imprimir_contacto) {
 
   return EXITO;
 } 
-
+/** Funcion auxiliar que imprime el contacto si todos los valores coinciden. */
 static void and_contacto(void* contacto, void* valores) {
   if (contacto_and(contacto, valores)) contacto_imprimir(contacto);
 }
@@ -289,6 +303,8 @@ Resultado and(Entorno entorno) {
   return and_or(entorno, and_contacto);
 }
 
+
+/** Funcion auxiliar que imprime el contacto si algun valor coincide. */
 static void or_contacto(void* contacto, void* valores) {
   if (contacto_or(contacto, valores)) contacto_imprimir(contacto);
 }
@@ -297,6 +313,8 @@ Resultado or(Entorno entorno) {
   return and_or(entorno, or_contacto);
 }
 
+
+/** Funciones comparadoras genericas por atributo. */
 static int comparar_nombre(void* contacto1, void* contacto2) {
   return contacto_comparar_nombre(contacto1, contacto2);
 }
@@ -309,6 +327,7 @@ static int comparar_edad(void* contacto1, void* contacto2) {
 static int comparar_telefono(void* contacto1, void* contacto2) {
   return contacto_comparar_telefono(contacto1, contacto2);
 }
+/** Funcion visitante generica para insertar contacto en el heap. */
 static void insertar_heap(void* contacto, void* heap) {
   heap_insertar(contacto, heap);
 }
@@ -319,6 +338,7 @@ Resultado guardar_ordenado(Entorno entorno) {
   if (fp == NULL) return ARCHIVO_ERROR;
   char* atributo = ingresar_string("nombre de atributo", STRLEN);
   FuncionComparadora comp;
+  // Determinamos que funcion comparadora usar.
   if (strcmp(atributosClave[NOMBRE], atributo) == 0) 
     comp = comparar_nombre;
   else if (strcmp(atributosClave[APELLIDO], atributo) == 0) 
@@ -328,6 +348,7 @@ Resultado guardar_ordenado(Entorno entorno) {
   else if (strcmp(atributosClave[TELEFONO], atributo) == 0) 
     comp = comparar_telefono;
   else {
+    // El atributo ingresado es invalido.
     fclose(fp); free(atributo); 
     return GUARDAR_ORDENADO_ATRUBUTO;
   }
@@ -343,12 +364,16 @@ Resultado guardar_ordenado(Entorno entorno) {
   return EXITO;
 }
 
+
+/** Funcion visitante generica para sumar las edades de los contactos. */
 static void sumar_edad(void* contacto, void* s) {
   *(int*)s += ((Contacto)contacto)->edad;
 }
+/** Funcion visitante generica para imprimir contacto. */
 static void imprimir_contacto(void* c, void* x __attribute__((unused))) { 
   contacto_imprimir(c); 
 }
+/** Funcion comparadora generica para comparar contacto por edad decreciente. */
 static int comparar_edad_decr(void* contacto1, void* contacto2) {
   return -contacto_comparar_edad(contacto1, contacto2); 
 }
@@ -356,13 +381,18 @@ static int comparar_edad_decr(void* contacto1, void* contacto2) {
 Resultado buscar_suma_edades(Entorno entorno) {
   unsigned int suma;
   printf("Ingrese un natural:\n>");
-  scanf("%u", &suma); while(getchar() != '\n'); 
+  scanf("%u", &suma); while(getchar() != '\n');
+  // La suma debe ser mayor a 0.
   if (suma == 0) return SUMA_EDADES_CERO;
 
   unsigned int ncontactos = tablahash_nelems(entorno.agenda);
+  // Si la agenda esta vacia, informamos que no se encontraron resultados.
   if (ncontactos == 0) return SUMA_EDADES_ENCONTRADO;
 
   unsigned int sumaEdades = 0;
+  // Sumamos las edades de todos los contactos.
+  // Si es menor a la suma, no hay resultado posible.
+  // Si es igual, imprimos todos los contactos.
   tablahash_recorrer(entorno.agenda, sumar_edad, &sumaEdades);
   if (suma > sumaEdades) return SUMA_EDADES_ENCONTRADO;
   else if (suma == sumaEdades) {
@@ -370,61 +400,86 @@ Resultado buscar_suma_edades(Entorno entorno) {
     return EXITO;
   }
 
+  // Optimizacion: ordenamos los contactos de mayor a menor edad
   Heap contactos = heap_crear(ncontactos, comparar_edad_decr);
   tablahash_recorrer(entorno.agenda, insertar_heap, contactos);
   
+  // Para cada subsuma entre 1 y suma, guardamos en mem la lista de contactos
+  // tal que sus edades suman la subsuma.
+  // Solo requerimos dos filas en la tabla: una para recordar los resultados
+  // anteriores, y una para guardar los calculos nuevos.
   Contacto** mem[2];
   mem[0] = malloc(sizeof(Contacto*) * (suma + 1)); assert(mem[0]);
   mem[1] = malloc(sizeof(Contacto*) * (suma + 1)); assert(mem[1]);
+  // Dejamos la primera tabla vacia.
+  memset(mem[0], 0, (suma + 1) * sizeof(Contacto*)); 
+  // En cant guardamos la cantidad de contactos en cada entrada de mem.
   unsigned int* cant[2];
   cant[0] = malloc(sizeof(int)* (suma + 1)); assert(cant[0]);
   cant[1] = malloc(sizeof(int)* (suma + 1)); assert(cant[0]);
-  memset(mem[0], 0, (suma + 1) * sizeof(Contacto*)); 
-  memset(cant[0], 0, (suma + 1) * sizeof(int));
 
   unsigned int sub;
   Contacto contacto;
   void* temp;
   int seguir = 1; unsigned int edad;
+  // Resolvemos el problema con estrategia bottom-up.
+  // Desde el mayor contacto al menor, chequeamos para cada subsuma:
+  //  - si ya tenemos una lista de contactos para esta subsuma, la copiamos
+  //  - si la edad del contacto es exactamente la subsuma, creamos una lista
+  //    con dicho contacto como unico elemento.
+  //  - si tenemos una lista de contactos para la subsuma - la edad,
+  //    la copiamos y agregamos el nuevo contacto.
+  // Luego de cada paso, chequeamos si ya tenemos una lista para la suma final.
+  // En ese caso, detenemos el proceso.
   while (seguir && !heap_vacio(contactos)) {
     contacto = heap_extraer(contactos);
     edad = contacto->edad;
     for (sub = 1; sub <= suma; sub++) {
+      // Ya existe una lista para esta subsuma: la copiamos.
       if (mem[0][sub]) {
         mem[1][sub] = mem[0][sub];
         cant[1][sub] = cant[0][sub];
       }
+      // La edad del contacto coincide, creamos una lista nueva.
       else if (edad == sub) {
         mem[1][sub] = malloc(sizeof(Contacto*) * ncontactos);
         assert(mem[1][sub]); 
         mem[1][sub][0] = contacto; 
         cant[1][sub] = 1;
       }
+      // Tenemos una subsuma menor: copiamos la lista y agregamos el contacto.
       else if (sub > edad && mem[0][sub - edad]) {
         mem[1][sub] = malloc(sizeof(Contacto) * ncontactos);
         assert(mem[1][sub]);
+        // Copiamos la lista.
         memcpy(mem[1][sub], mem[0][sub - edad], 
           cant[0][sub - edad] * sizeof(Contacto));
+        // Agregamos el contacto nuevo.
         mem[1][sub][cant[0][sub - edad]] = contacto;
+        // Actualizamos la cantidad.
         cant[1][sub] = cant[0][sub - edad] + 1;
       }
-      else {
-        mem[1][sub] = NULL; cant[1][sub] = 0;
-      }
+      // No hay coincidencias, dejamos la entrada vacia.
+      else mem[1][sub] = NULL;
     }
     temp = mem[0]; mem[0] = mem[1]; mem[1] = temp;
     temp = cant[0]; cant[0] = cant[1]; cant[1] = temp;
+    // Encontramos la lista que estabamos buscando: salimos del loop.
     if (mem[0][suma]) seguir = 0;
   }
   heap_destruir(contactos);
 
   Resultado res = EXITO;
+  // Si no hubo resultados, informamos la situacion.
   if (!mem[0][suma]) res = SUMA_EDADES_ENCONTRADO;
+  // Imprimimos el resultado encontrado.
   else for (unsigned int i = 0; i < cant[0][suma]; i++)
     contacto_imprimir(mem[0][suma][i]); 
+  // Liberamos la memoria utilizada.
   for (sub = 1; sub <= suma; sub++)
     free(mem[0][sub]);
   free(mem[0]); free(mem[1]);
   free(cant[0]); free(cant[1]);
+  
   return res;
 }
--- src/operaciones.h
+++ src/operaciones.h
@@ -4,45 +4,110 @@
 #include "historial.h"
 #include "estructuras/tablahash.h"
 
+/**
+ * Estructura de entorno de sesion. Tenemos una agenda de contactos
+ * y un historial de operaciones.
+ */
 typedef struct {
-  TablaHash tabla;
+  TablaHash agenda;
   Historial historial;
 } Entorno;
-
+/**
+ * Tipo de resultado obtenido luego de realizar una operacion.
+ */
 typedef enum {
-  EXITO,
-  BUSCAR_EXISTE,
-  AGREGAR_EXISTE,
-  ELIMINAR_EXISTE,
-  EDITAR_EXISTE,
-  ARCHIVO_ENCONTRADO,
-  ARCHIVO_ERROR,
-  CARGAR_COMA,
-  CARGAR_SALTO_LINEA,
-  CARGAR_CABECERA,
-  CARGAR_VACIO,
-  CARGAR_EOF,
-  CARGAR_LARGO,
-  CARGAR_FORMATO,
-  DESHACER_VACIO,
-  REHACER_VACIO,
-  AND_OR_VACIO,
-  GUARDAR_ORDENADO_ATRUBUTO,
-  SUMA_EDADES_CERO,
-  SUMA_EDADES_ENCONTRADO,
+  EXITO,                        // Exito
+  BUSCAR_EXISTE,                // No existe el contacto requerido.
+  AGREGAR_EXISTE,               // El contacto a agregar ya existe.
+  ELIMINAR_EXISTE,              // No existe el contacto requerido.
+  EDITAR_EXISTE,                // No existe el contacto requerido.
+  ARCHIVO_ENCONTRADO,           // No se ha encontrado el archivo.
+  ARCHIVO_ERROR,                // Error al intentar abrir el archivo.
+  CARGAR_COMA,                  // Coma inesperada en el archivo.
+  CARGAR_SALTO_LINEA,           // Salto de linea inesperado en el archivo.
+  CARGAR_CABECERA,              // Cabecera invalida en el archivo.
+  CARGAR_VACIO,                 // Archivo vacio.
+  CARGAR_EOF,                   // Final del archivo inesperado.
+  CARGAR_LARGO,                 // Dato inesperadamente largo.
+  CARGAR_FORMATO,               // Formato de CSV incorrecto.
+  DESHACER_VACIO,               // No hay operaciones para deshacer.
+  REHACER_VACIO,                // No hay operaciones para rehacer.
+  AND_OR_VACIO,                 // Todos los campos vacios.
+  GUARDAR_ORDENADO_ATRUBUTO,    // Atributo invalido.
+  SUMA_EDADES_CERO,             // La suma no puede ser cero.
+  SUMA_EDADES_ENCONTRADO,       // Busqueda infructuosa.
 } Resultado;
 
+/**
+ * Pide por entrada estandar nombre y apellido del contacto.
+ * Busca el contacto en la agenda y lo imrime en pantalla.
+ * Informa si no lo encontro, devuelve EXITO en caso contrario.
+ */
 Resultado buscar(Entorno entorno);
+/**
+ * Pide por entrada estandar los datos de un contacto nuevo.
+ * Agrega el contacto a la agenda. Informa si el contacto ya existia,
+ * devuelve EXITO en caso contrario y agrega la operacion al historial.
+ */
 Resultado agregar(Entorno entorno);
+/**
+ * Pide por entrada estandar nombre y apellido del contacto.
+ * Busca el contacto en la agenda y lo elimina. Informa si no lo encontro,
+ * devuelve EXITO en caso contrario y agrega la operacion al historial.
+ */
 Resultado eliminar(Entorno entorno);
+/**
+ * Pide por entrada estandar nombre y apellido del contacto.
+ * Busca el contacto en la agenda y, de encontrarlo, pide el resto de los
+ * datos, lo modifica, agrega la operacion al historial y devuelve EXITO.
+ */
 Resultado editar(Entorno entorno);
+/**
+ * Pide por entrada estandar ruta del archivo y carga los datos en la agenda.
+ * Para cada contacto, informa si se ha insertado correctamente o si el contacto
+ * ya se encontraba en la agenda. Informa si no pudo abrir el archivo o si se
+ * interrumpio su lectura, o devuelve EXITO en caso contrario.
+ */
 Resultado cargar(Entorno entorno);
+/**
+ * Pide por entrada estandar ruta del archivo. Informa si no pudo abrir
+ * el archivo. En caso contrario, guarda los datos de la agenda y devuelve
+ * EXITO. 
+ */
 Resultado guardar(Entorno entorno);
+/**
+ * De no estar vacio el historial, deshace la ultima operacion realizada.
+ */
 Resultado deshacer(Entorno entorno);
+/**
+ * De no estar vacio el historial, rehace la ultima operacion deshecha.
+ */
 Resultado rehacer(Entorno entorno);
+/**
+ * Pide por entrada estandar un valor para cada atributo de contacto.
+ * En caso de haber uno no vacio, imprime por pantalla los contactos
+ * que cumplan con todos los valores requeridos.
+ */
 Resultado and(Entorno entorno);
+ /**
+  * Pide por entrada estandar un valor para cada atributo de contacto.
+  * En caso de haber uno no vacio, imprime por pantalla los contactos
+  * que cumplan con alguno de los valores requeridos.
+  */
 Resultado or(Entorno entorno);
+/**
+ * Pide por entrada estandar una ruta de archivo y un atributo de contacto.
+ * Informa en caso de no poder abrir el archivo o de ser invalido el atributo.
+ * En caso contrario, guarda los datos de la agenda en el archivo ordenados
+ * crecientemente por el atributo ingresado.
+ */
 Resultado guardar_ordenado(Entorno entorno);
+/**
+ * Pide por entrada estandar un natural. Informa si el valor es 0.
+ * En caso contrario, busca un subconjunto de contactos tal que sus edades
+ * sumen el numero ingresado. Informa en caso de no encontrarlo, o imprime
+ * por pantalla los contactos y devuelve EXITO en caso contrario.
+ */
 Resultado buscar_suma_edades(Entorno entorno);
 
 
